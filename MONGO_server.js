888d4d56ab0251544f57749af8eafaaefe7bb766
Matan Konstantini <matanko35@gmail.com>
2023-09-05 12:17:53 +0300

    updated project

--- MONGO_server.js
+++ MONGO_server.js
@@ -3,14 +3,21 @@ const app = express();
 
 // PORT
 require("dotenv").config();
-const port = process.env.port || 8001;
-app.listen(port, () => {
-    console.log("connected to port", port);
-})
+const port = process.env.PORT || 8001;
+app.listen(port, () => console.log("connected to port", port));
 
 // API
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.get("/api", (req, res) => {
-    res.send("welcome to root")
+    res.send("welcome to root");
 })
+
+// DB - Mongoose
+// connect
+const mongoose = require("mongoose")
+mongoose
+    .connect(process.env.DB, {useNewUrlParser: true}) 
+    .then(() => console.log("connected to database"))
+    .catch((err) => console.log(err))
+
